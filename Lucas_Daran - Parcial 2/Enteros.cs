b006cc648550413d921fa58c8e8b0a5c59c7a9b0
lucas <lucas@DESKTOP-5TLC2TC>
2023-05-30 19:11:20 -0300

    Agregar archivos de proyecto.

---
+++ Lucas_Daran - Parcial 2/Enteros.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucas_Daran___Parcial_2
+{
+    internal class Enteros
+    {
+
+        private const int MaxElementos = 100;
+        private int[] arreglo;
+        private int cantidadElementos;
+
+        public Enteros()
+        {
+            arreglo = new int[MaxElementos];
+            cantidadElementos = 0;
+        }
+
+        public int CantidadElementos
+        {
+            get { return cantidadElementos; }
+        }
+
+        public int Add(int elemento)
+        {
+            if (cantidadElementos < MaxElementos)
+            {
+                arreglo[cantidadElementos] = elemento;
+                cantidadElementos++;
+                return cantidadElementos - 1;
+            }
+            else
+            {
+                Console.WriteLine("El arreglo está lleno. No se puede agregar más elementos.");
+                return -1;
+            }
+        }
+
+        public int GetAt(int index)
+        {
+            if (index >= 0 && index < cantidadElementos)
+            {
+                return arreglo[index];
+            }
+            else
+            {
+                Console.WriteLine("Índice fuera de rango. No se puede obtener el elemento.");
+                return -1; // Valor sentinela para indicar un error
+            }
+        }
+
+        public void SetAt(int elemento, int index)
+        {
+            if (index >= 0 && index < cantidadElementos)
+            {
+                arreglo[index] = elemento;
+            }
+            else
+            {
+                Console.WriteLine("Índice fuera de rango. No se puede establecer el elemento.");
+            }
+        }
+
+        public void RemoveAll()
+        {
+            cantidadElementos = 0;
+        }
+
+        public int GetLength()
+        {
+            return cantidadElementos;
+        }
+
+        public bool IsEmpty()
+        {
+            return cantidadElementos == 0;
+        }
+
+        public void ImprimirArreglo()
+        {
+            Console.WriteLine("Elementos en el arreglo:");
+            for (int i = 0; i < cantidadElementos; i++)
+            {
+                Console.WriteLine(arreglo[i]);
+            }
+        }
+
+    }
+}
---
+++ Lucas_Daran - Parcial 2/Program.cs
@@ -0,0 +1,61 @@
+﻿using Lucas_Daran___Parcial_2;
+
+namespace parcial2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Enteros enteros = new Enteros();
+
+            Console.WriteLine("----Menu----");
+            Console.WriteLine("1. Agregar Elementos");
+            Console.WriteLine("2. Mostrar Arreglo");
+            Console.WriteLine("3. Mostrar Cantidad de Elementos del Arreglo");
+            Console.WriteLine("4. Busqueda por Indice");
+            Console.WriteLine("5. Editar Arreglo");
+            Console.WriteLine("6. Estado del Arreglo");
+            Console.WriteLine("7. Eliminar todos los elementos del Arreglo");
+
+            int opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1: 
+            }
+
+
+            //agregar
+            enteros.Add(10);
+            enteros.Add(20);
+            enteros.Add(30);
+            //
+
+            //mostrar array
+            enteros.ImprimirArreglo();
+            //
+
+            //busqueda index
+            int elemento = enteros.GetAt(1);
+            Console.WriteLine("Elemento en la posición 1: " + elemento);
+            //
+
+
+            enteros.SetAt(40, 2);
+            enteros.ImprimirArreglo();
+
+            int cantidadElementos = enteros.GetLength();
+            Console.WriteLine("Cantidad de elementos: " + cantidadElementos);
+
+            bool vacio = enteros.IsEmpty();
+            Console.WriteLine("¿El arreglo está vacío?: " + vacio);
+
+            enteros.RemoveAll();
+            enteros.ImprimirArreglo();
+
+            vacio = enteros.IsEmpty();
+            Console.WriteLine("¿El arreglo está vacío?: " + vacio);
+
+        }
+    }
+}
