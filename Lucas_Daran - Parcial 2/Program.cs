83689bbf62d774e6ac52cddf40e70282ba55687b
lucas <lucas@DESKTOP-5TLC2TC>
2023-06-01 17:02:36 -0300

    corregido, falta pulir

--- Lucas_Daran - Parcial 2/Program.cs
+++ Lucas_Daran - Parcial 2/Program.cs
@@ -8,54 +8,153 @@ namespace parcial2
         {
             Enteros enteros = new Enteros();
 
-            Console.WriteLine("----Menu----");
-            Console.WriteLine("1. Agregar Elementos");
-            Console.WriteLine("2. Mostrar Arreglo");
-            Console.WriteLine("3. Mostrar Cantidad de Elementos del Arreglo");
-            Console.WriteLine("4. Busqueda por Indice");
-            Console.WriteLine("5. Editar Arreglo");
-            Console.WriteLine("6. Estado del Arreglo");
-            Console.WriteLine("7. Eliminar todos los elementos del Arreglo");
-
-            int opcion = int.Parse(Console.ReadLine());
-
-            switch (opcion)
+            bool volver = true;
+            do
             {
-                case 1: 
-            }
+                Console.WriteLine("----Menu----");
+                Console.WriteLine("1. Agregar Elementos");
+                Console.WriteLine("2. Mostrar Arreglo");
+                Console.WriteLine("3. Mostrar Cantidad de Elementos del Arreglo");
+                Console.WriteLine("4. Busqueda por Indice");
+                Console.WriteLine("5. Editar Arreglo");
+                Console.WriteLine("6. Estado del Arreglo");
+                Console.WriteLine("7. Eliminar todos los elementos del Arreglo");
+                Console.WriteLine("0. Salir");
 
+                int opcion = int.Parse(Console.ReadLine());
+            
+                bool vacio = enteros.IsEmpty();
 
-            //agregar
-            enteros.Add(10);
-            enteros.Add(20);
-            enteros.Add(30);
-            //
+                bool continuar = true;
 
-            //mostrar array
-            enteros.ImprimirArreglo();
-            //
+                while (continuar)
+                {
+                        switch (opcion)
+                        {
+                            case 1:
+                                Console.WriteLine("Ingrese la cantidad de elementos para añadir");
+                                int cantElementosIngr = int.Parse(Console.ReadLine());
 
-            //busqueda index
-            int elemento = enteros.GetAt(1);
-            Console.WriteLine("Elemento en la posición 1: " + elemento);
-            //
+                                for (int i = 0; i < cantElementosIngr; i++)
+                                {
+                                    Console.WriteLine("Ingrese Elemento N°: " + (i));
+                                    int ingresoElemento = int.Parse(Console.ReadLine());
+                                    enteros.Add(ingresoElemento);
+                                }
+                                Console.WriteLine("~Los Elementos se añadieron correctamente~");
+                                Cierre();
+                                continuar = false;
+                                break;
 
+                            case 2:
 
-            enteros.SetAt(40, 2);
-            enteros.ImprimirArreglo();
+                                if (vacio)  //enteros.CantidadElementos == 0
+                                {
+                                    Console.WriteLine("El arreglo no tiene elementos");
+                                }
+                                else
+                                {
+                                    enteros.ImprimirArreglo();
+                                }
+                                Cierre();
+                                continuar = false;
+                                break;
 
-            int cantidadElementos = enteros.GetLength();
-            Console.WriteLine("Cantidad de elementos: " + cantidadElementos);
+                            case 3:
 
-            bool vacio = enteros.IsEmpty();
-            Console.WriteLine("¿El arreglo está vacío?: " + vacio);
+                                int cantidadElementos = enteros.GetLength();
+                                Console.WriteLine("Cantidad de elementos: " + cantidadElementos);
+                                Cierre();
+                                continuar = false;
+                                break;
 
-            enteros.RemoveAll();
-            enteros.ImprimirArreglo();
+                            case 4:
 
-            vacio = enteros.IsEmpty();
-            Console.WriteLine("¿El arreglo está vacío?: " + vacio);
+                                Console.WriteLine("Ingrese el indice a buscar");
+                                int indexBuscado = int.Parse(Console.ReadLine());
+                                int buscarElemento = enteros.GetAt(indexBuscado);
+                                Console.WriteLine($"Elemento en la posición {indexBuscado}: " + buscarElemento);
+                                Cierre();
+                                continuar = false;
+                                break;
 
+                            case 5:
+
+                                if (enteros.CantidadElementos == 0)
+                                {
+                                    Console.WriteLine("Imposible editar porque el arreglo esta vacio");                           
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Ingrese -Indice a Editar-");
+                                    int indiceEdit = int.Parse(Console.ReadLine());
+                                    Console.WriteLine("Ingrese -Elemento nuevo-");
+                                    int elementoEdit = int.Parse(Console.ReadLine());
+
+                                    int cantElementos = enteros.GetLength();
+                                    if (indiceEdit > cantElementos)
+                                    {
+                                        Console.WriteLine("Error. Indice incorrecto");
+                                    }
+                                    else
+                                    {
+                                        enteros.SetAt(elementoEdit, indiceEdit);
+                                        Console.WriteLine("~El arreglo se edito correctamente~");
+                                    }
+                                }
+                                Cierre();
+                                continuar = false;
+                                break;
+
+                            case 6:
+                            
+                                Console.WriteLine("¿El arreglo está vacío?: " + vacio);
+                                Cierre();
+                                continuar = false;
+                                break;
+
+                            case 7:
+                                Console.WriteLine("Estas seguro de borrar todos los elementos del arreglo? si/no");
+                                string resp = Console.ReadLine();
+
+                                if (resp != "si" && resp != "no")
+                                {
+                                    Console.WriteLine("Error de tipeo");
+                                }
+                                else if (resp == "no")
+                                {
+                                    Console.WriteLine("Borrado Incompleto. Presione -Enter- para volver al menu principal");
+                                    continuar = false;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("~Los elementos se eliminaron correctamente~");
+                                    enteros.RemoveAll();
+                                }
+                                Cierre();
+                                continuar = false;
+                                break;
+
+                        case 0:
+                            Environment.Exit(1);
+                            break;
+
+                        default:
+                            Console.WriteLine("Error. Vuelva a Ingresar");
+                            Cierre();
+                            continuar = false;
+                            break;
+                        }        
+                }
+            } while (volver = true);
         }
+
+        private static void Cierre()
+        { 
+            Console.ReadKey();
+            Console.Clear();            
+        }
+            
+
     }
 }
